403e99d69706f9c341e0793c3135dc05de19b3a0
Rodrigo Machado <machadorodrigo987@gmail.com>
2026-06-04 18:33:46 +0100

    estrutura para entrega

--- delivery/DEMO-v1/phase1-script.sh
+++ delivery/DEMO-v1/phase1-script.sh
@@ -5,28 +5,28 @@
 echo "==================================================="
 echo "Example 1"
 echo "==================================================="
-java -jar project-v1.jar input/ex1-deck.txt input/ex1-script.txt 2 > output/out1.txt
+java -jar project-v1.jar DEMO-v1/input/ex1-deck.txt DEMO-v1/input/ex1-script.txt 2 > output/out1.txt
 
 
 echo "==================================================="
 echo "Example 2"
 echo "==================================================="
-java -jar project-v1.jar input/ex2-deck.txt input/ex2-script.txt 3 > output/out2.txt
+java -jar project-v1.jar DEMO-v1/input/ex2-deck.txt DEMO-v1/input/ex2-script.txt 3 > output/out2.txt
 
 
 echo "==================================================="
 echo "Example 3"
 echo "==================================================="
-java -jar project-v1.jar input/ex3-deck.txt input/ex3-script.txt 4 > output/out3.txt
+java -jar project-v1.jar DEMO-v1/input/ex3-deck.txt DEMO-v1/input/ex3-script.txt 4 > output/out3.txt
 
 
 echo "==================================================="
 echo "Example 4"
 echo "==================================================="
-java -jar project-v1.jar input/ex4-deck.txt input/ex4-script.txt 5 > output/out4.txt
+java -jar project-v1.jar DEMO-v1/input/ex4-deck.txt DEMO-v1/input/ex4-script.txt 5 > output/out4.txt
 
 
 echo "==================================================="
 echo "Example 5"
 echo "==================================================="
-java -jar project-v1.jar input/ex5-deck.txt input/ex5-script.txt 6 > output/out5.txt
+java -jar project-v1.jar DEMO-v1/input/ex5-deck.txt DEMO-v1/input/ex5-script.txt 6 > output/out5.txt
